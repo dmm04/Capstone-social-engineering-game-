0740ae1b18f03e820f1bbefc7dd914b6db9375e0
Philip Gonzales <philip08533gonzales@gmail.com>
2025-10-27 08:49:03 -0400

    Add files via upload

---
+++ capstone/Godot_Game_Files/Assets/Buttons/!alert.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+@onready var sprite = $AnimatedSprite2D
+@onready var message_label = $AlertLabel
+
+var player_in_area := false
+
+func _ready():
+	# Start the popup animation
+	sprite.play("float")
+
+# Must include delta parameter to match parent signature
+func _process(delta: float) -> void:
+	# Only check for interact press if player is in the area
+	if player_in_area and Input.is_action_just_pressed("interact"):
+		open_puzzle_scene()
+
+func _on_body_entered(body):
+		player_in_area = true
+		message_label.visible = true
+		print("Body Entered")
+
+func _on_body_exited(body):
+		player_in_area = false
+		message_label.visible = false
+
+func open_puzzle_scene():
+	get_tree().change_scene_to_file("res://capstone/Godot_Game_Files/Puzzles/pc_display.tscn")
