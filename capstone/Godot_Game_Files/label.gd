5692068d2710281e7e6059b68b29ee7bd6d68658
Philip Gonzales <philip08533gonzales@gmail.com>
2025-11-05 22:48:54 -0500

    tried and failed to get stuff working.

---
+++ capstone/Godot_Game_Files/label.gd
@@ -0,0 +1,2 @@
+extends Label
+$Label.rect_scale = Vector2(0.5, 0.5)  # Shrinks the label to 50%
--- capstone/canvas_layer.gd
+++
@@ -1,2 +0,0 @@
-extends CanvasLayer
-get_tree().current_scene.get_node("UI").add_child(puzzle_scene)
