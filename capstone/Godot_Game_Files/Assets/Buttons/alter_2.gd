53ff0ffa71fffbb35659bc879684d88741c996bb
Philip Gonzales <philip08533gonzales@gmail.com>
2025-11-07 11:21:11 -0500

    trying to get collision to work how i want

--- capstone/Godot_Game_Files/Assets/Buttons/alter_2.gd
+++ capstone/Godot_Game_Files/Assets/Buttons/alter_2.gd
@@ -1,28 +1,42 @@
 extends Area2D
 
 @onready var sprite = $AnimatedSprite2D
+@onready var message_label = $AlertLabel
 
-var player_in_area = false
+var player_in_area2 := false
+
 
 func _ready():
 	# Start the popup animation
 	sprite.play("float")
 	$AlertLabel.visible = false
+	$"../../Alert".set_process(false)
+
 
 # Must include delta parameter to match parent signature
 func _process(delta: float) -> void:
 	# Only check for interact press if player is in the area
-	if player_in_area and Input.is_action_just_pressed("interact"):
-		open_puzzle_scene()
+	if player_in_area2 and Input.is_action_just_pressed("interact"):
+		continue_text()
+	
 
 func _on_body_entered(body):
-		player_in_area = true
-		$AlertLabel.visible = true
+		player_in_area2 = true
+		message_label.visible = true
 		print("Body Entered")
 
 func _on_body_exited(body):
-		player_in_area = false
-		$AlertLabel.visible = false
+		player_in_area2 = false
+		message_label.visible = false
 
-func open_puzzle_scene():
-	get_tree().change_scene_to_file("res://Godot_Game_Files/Puzzles/emails/phising emails/phising_email.tscn")
+func continue_text():
+	$"../Label".text = "When you see a ! above your coworkers, you need to go check out thier computer and complete the cybersecurity task! Good luck! DONT DISAPOINT ME."
+	continuei() 
+func continuei():  
+	if player_in_area2 and Input.is_action_just_pressed("interact"):
+		$"../Label".visible = false
+		$".".visible = false
+		$"../Chatbubble".visible = false 
+		$"../../Alert".set_process(true)
+		$"../../Alert".visible =true
+	
