ab8c85f39dc2417a10fd70a18b913918b2563462
Philip Gonzales <philip08533gonzales@gmail.com>
2026-04-01 01:55:01 -0400

    endscene stuffs

--- capstone/Godot_Game_Files/Puzzles/Phil/endcutscene/end_camera.gd
+++ capstone/Godot_Game_Files/Puzzles/Phil/endcutscene/end_camera.gd
@@ -1,11 +1,14 @@
 extends Camera2D
 
-@export var target_x := 0
+@export var target_x := 120
 @export var speed := 200
 
 func _process(delta):
 	if Dialogic.VAR.get("end_step") == 1:
 		if position.x < target_x:
 			position.x += speed * delta
+			
 		else:
-			Dialogic.VAR.set("end_step", 1.1)
+			pass
+		Dialogic.VAR.set("end_step", 1.1)
+		Dialogic.start("endcutscene/timeline")
---
+++ capstone/Godot_Game_Files/endcutscene/shareholder.gd
@@ -0,0 +1,24 @@
+extends Node2D
+"""
+func _ready():
+	pass
+
+func walk():
+	$member.play("walk")
+	$member2.play("walk")
+	$member3.play("walk")
+	$member4.play("walk") 
+
+func _process(delta):
+	pass
+	if $member.position.x < 250 and $member2.position.x < 250 and $member3.position.x < 250 and $member4.position.x < 250  :
+		$member.position.x += 200 * delta
+		$member2.position.x += 200 * delta
+		$member3.position.x += 200 * delta
+		$member4.position.x += 200 * delta
+	else:
+		$member.play("stand")
+		$member2.play("stand")
+		$member3.play("stand")
+		$member4.play("stand")
+"""
