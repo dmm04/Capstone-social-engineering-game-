19e7130a35fd41740a6bb69aacf6cb83fa33d6df
Philip Gonzales <philip08533gonzales@gmail.com>
2026-04-03 11:44:08 -0400

    fun stuff

--- capstone/Godot_Game_Files/Puzzles/Phil/endcutscene/endcutscene.gd
+++ capstone/Godot_Game_Files/Puzzles/Phil/endcutscene/endcutscene.gd
@@ -1,14 +1,9 @@
 extends Node2D
 func _ready():
-	$mainchar/player.play("walk")
-	create_tween().tween_property($mainchar/player, "position:x", 1300, 3).finished.connect(func():
-		$mainchar/player.play("idle")
-	)
+	$mainchar/player.play("fall")
+	$mainchar/player.play("idle")
+	Dialogic.start("endcutscene/emilyendscene")
+	
 
-	$lady/lucy.play("walk")
-	create_tween().tween_property($lady/lucy, "position:x", 1300, 3).finished.connect(func():
-		$lady/lucy.play("idle")
-		Dialogic.start("endcutscene/timeline")
-	)
 
 	
